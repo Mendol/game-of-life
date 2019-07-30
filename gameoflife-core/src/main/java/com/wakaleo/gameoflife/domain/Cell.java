3a209deffbc6aeaaa4fabe76755f629d4081660d
Mendel <wangmengdi025@163.com>
2019-07-30 13:52:20 +0800

    Update Cell.java

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -9,9 +9,9 @@ package com.wakaleo.gameoflife.domain;
  */
 public enum Cell {
 	// Symbols to represent cell status
-    LIVE_CELL("+"), DEAD_CELL(".");
+    LIVE_CELL("*"), DEAD_CELL(".");
 
-    private String symbol
+    private String symbol;
 
     private Cell(final String initialSymbol) {
         this.symbol = initialSymbol;
